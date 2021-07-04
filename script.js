07b26c0cfe4f8f8f9ac4e180d6d07bbe60d7359a
James Hooper <jameshooperdesigns@gmail.com>
2021-07-04 15:13:12 -0600

    Add logic to the script.js and styles.css for the new custom cursor

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function cursor(e) {
   mouseCursor.style.top = e.pageY + 'px';
   mouseCursor.style.left = e.pageX + 'px';
 }
+
+navLinks.forEach(link => {
+  link.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove('link-grow');
+    link.classList.remove('hovered-link');
+  })
+  link.addEventListener('mouseover', () => {
+    mouseCursor.classList.add('link-grow');
+    link.classList.add('hovered-link');
+  })
+});
